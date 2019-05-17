a8af0192eda6d3242723872a692aebeb150af766
Mathieu Poumeyrol <kali@zoy.org>
2019-05-17 16:47:53 +0200

    first run

--- core/src/framework.rs
+++ core/src/framework.rs
@@ -5,16 +5,21 @@ use std::fmt::Debug;
 use std::io::Read;
 use std::path::Path;
 
-pub type OpBuilder<ProtoOp> = fn(&ProtoOp) -> TractResult<Box<Op>>;
+pub type OpBuilder<ProtoOp, O> = fn(&ProtoOp) -> TractResult<O>;
 
-#[derive(Default)]
-pub struct OpRegister<ProtoOp>(HashMap<String, OpBuilder<ProtoOp>>);
+pub struct OpRegister<ProtoOp, O>(HashMap<String, OpBuilder<ProtoOp, O>>);
 
-impl<ProtoOp> OpRegister<ProtoOp> {
-    pub fn get(&self, name: &str) -> Option<&OpBuilder<ProtoOp>> {
+impl<ProtoOp, O> Default for OpRegister<ProtoOp, O> {
+    fn default() -> OpRegister<ProtoOp, O> {
+        OpRegister(HashMap::new())
+    }
+}
+
+impl<ProtoOp, O> OpRegister<ProtoOp, O> {
+    pub fn get(&self, name: &str) -> Option<&OpBuilder<ProtoOp, O>> {
         self.0.get(name)
     }
-    pub fn insert(&mut self, name: impl AsRef<str>, b: OpBuilder<ProtoOp>) {
+    pub fn insert(&mut self, name: impl AsRef<str>, b: OpBuilder<ProtoOp, O>) {
         self.0.insert(name.as_ref().to_string(), b);
     }
     pub fn names(&self) -> impl Iterator<Item = &str> {
@@ -22,8 +27,8 @@ impl<ProtoOp> OpRegister<ProtoOp> {
     }
 }
 
-pub trait Framework<ProtoOp: Debug, ProtoModel: Debug> {
-    fn op_builder_for_name(&self, name: &str) -> Option<&OpBuilder<ProtoOp>>;
+pub trait Framework<ProtoOp: Debug, O: From<UnimplementedOp>, ProtoModel: Debug> {
+    fn op_builder_for_name(&self, name: &str) -> Option<&OpBuilder<ProtoOp, O>>;
     fn proto_model_for_read(&self, reader: &mut Read) -> TractResult<ProtoModel>;
     fn model_for_proto_model(&self, proto: &ProtoModel) -> TractResult<InferenceModel>;
 
@@ -44,10 +49,9 @@ pub trait Framework<ProtoOp: Debug, ProtoModel: Debug> {
         self.model_for_read(&mut r)
     }
 
-    fn build_op(&self, name: &str, payload: &ProtoOp) -> TractResult<Box<Op>> {
-        match self.op_builder_for_name(name) {
+    fn build_op(&self, name: &str, payload: &ProtoOp) -> TractResult<O> {        match self.op_builder_for_name(name) {
             Some(builder) => builder(payload),
-            None => Ok(Box::new(UnimplementedOp::new(name, format!("{:?}", payload)))),
+            None => Ok(UnimplementedOp::new(name, format!("{:?}", payload)).into()),
         }
     }
 }
